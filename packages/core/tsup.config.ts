c7255bc1bd915e7006750211401102c1c49a6b2a
Sagiri Ikeda <62353994+SagiriIkeda@users.noreply.github.com>
2025-07-26 17:38:18 -0400

    fix(core): forgot to push tsup config and base.scss

---
+++ packages/core/tsup.config.ts
@@ -0,0 +1,22 @@
+import { sassPlugin } from 'esbuild-sass-plugin';
+import { defineConfig } from 'tsup';
+
+export default [
+	defineConfig({
+		target: 'es2022',
+		clean: true,
+		sourcemap: true,
+		keepNames: true,
+		tsconfig: 'src/tsconfig.json',
+		entry: ['src/index.ts', 'src/styles/base.scss'],
+		outDir: 'dist/bundle',
+		bundle: true,
+		platform: 'browser',
+		minify: true,
+		external: [],
+		noExternal: ['lit', '@lit/context', 'highlight.js'],
+		format: 'esm',
+		esbuildPlugins: [sassPlugin()],
+		outExtension: () => ({ js: '.mjs' })
+	})
+];
