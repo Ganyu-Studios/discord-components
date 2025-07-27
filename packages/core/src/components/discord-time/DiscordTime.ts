4d74a153db43750ad11050af3fee37758873980b
Sagiri Ikeda <62353994+SagiriIkeda@users.noreply.github.com>
2025-07-27 13:52:37 -0400

    fix(core/discord-time): precision with hours and days

--- packages/core/src/components/discord-time/DiscordTime.ts
+++ packages/core/src/components/discord-time/DiscordTime.ts
@@ -19,17 +19,17 @@ const DATE_TYPE_FORMATS = {
 const RELATIVE_DATE_CONVERSION = {
 	second: [1_000],
 	minute: [60_000],
-	hour: [3_600_000],
-	day: [86_400_000],
+	hour: [3_600_000, 0.75],
+	day: [86_400_000, 0.883, 21.2 * 3_600_000],
 	week: [604_800_000, 0.5],
 	month: [2_419_200_000, 0.6],
 	year: [29_030_400_000, 0.7]
-} as const;
+} as const satisfies Record<string, [value: number, rounder?: number, minTake?: number]>;
 
 const MAX_NEXT_UPDATE_DELAY = 2_147_483_647; // 2 ^ 31 - 1 milliseconds (about 24 days)
 
 const ENTRIES_RELATIVE_DATE_CONVERSION = Object.entries(RELATIVE_DATE_CONVERSION);
-const formatter = new Intl.RelativeTimeFormat(undefined, { numeric: 'auto' });
+const formatter = new Intl.RelativeTimeFormat();
 
 @customElement('discord-time')
 export class DiscordTime extends LitElement {
@@ -68,16 +68,16 @@ export class DiscordTime extends LitElement {
 		window.requestAnimationFrame(() => {
 			if (this.format === 'R') {
 				const [time, nextUpdateIn] = this.getRelativeTime(this.timestamp);
+				this.resultTime = time;
+				if (this.baseNow) return;
 				this.timeout = window.setTimeout(() => {
 					this.parseTime();
 				}, nextUpdateIn);
-				this.resultTime = time;
-				return time;
+				return;
 			}
 
 			const date = new Date(this.timestamp * 1_000);
 			this.resultTime = date.toLocaleString(undefined, DATE_TYPE_FORMATS[this.format]);
-			return this.resultTime;
 		});
 	}
 
@@ -85,19 +85,20 @@ export class DiscordTime extends LitElement {
 		const time = timestamp * 1_000;
 		const date = new Date(time);
 		const now = this.baseNow ?? Date.now();
+		// ? this is negative when the time is in the future, in the past it's positive
 		const diff = now - date.getTime();
 		const absDiff = Math.abs(diff);
 
-		if (diff >= 0 && diff < 5_000) {
-			return [formatter.format(0, 'second'), 5_000 - diff];
+		if (diff >= 0 && diff < 1_000) {
+			return [formatter.format(-1, 'second'), 1_000 - diff];
 		}
 
 		let unitTarget: keyof typeof RELATIVE_DATE_CONVERSION = 'second';
 		let unitValueTarget = 1_000;
 		let roundProportion: number | undefined;
 
-		for (const [unit, [value, rProportion]] of ENTRIES_RELATIVE_DATE_CONVERSION) {
-			if (absDiff > value) {
+		for (const [unit, [value, rProportion, minTake]] of ENTRIES_RELATIVE_DATE_CONVERSION) {
+			if (absDiff > value || (minTake && absDiff > minTake)) {
 				unitTarget = unit as keyof typeof RELATIVE_DATE_CONVERSION;
 				unitValueTarget = value;
 				roundProportion = rProportion;
@@ -110,15 +111,22 @@ export class DiscordTime extends LitElement {
 		const rawUnitValue = diff / unitValueTarget;
 		const absUnitValue = Math.abs(rawUnitValue);
 		const absFloored = Math.floor(absUnitValue);
+
 		const unitValueDecimals = absUnitValue - absFloored;
+
 		const rounded =
-			(diff < 0 ? Math.ceil(rawUnitValue) : Math.floor(rawUnitValue)) +
-			(roundProportion ? (unitValueDecimals >= roundProportion ? (diff < 0 ? -1 : 1) : 0) : 0);
+			absUnitValue < 1
+				? rawUnitValue < 0
+					? -1
+					: 1
+				: (diff < 0 ? Math.ceil(rawUnitValue) : Math.floor(rawUnitValue)) +
+					(roundProportion ? (unitValueDecimals >= roundProportion ? (diff < 0 ? -1 : 1) : 0) : 0);
 
-		const proportion = diff < 0 ? unitValueDecimals : 1 - unitValueDecimals;
+		const proportion = absUnitValue < 1 ? 1 : diff < 0 ? unitValueDecimals : 1 - unitValueDecimals;
 		const nextUpdateIn = Math.min(proportion * unitValueTarget, MAX_NEXT_UPDATE_DELAY);
+		const formatted = formatter.format(-rounded, unitTarget);
 
-		return [formatter.format(-rounded, unitTarget), nextUpdateIn];
+		return [formatted, nextUpdateIn];
 	}
 
 	public override connectedCallback() {
